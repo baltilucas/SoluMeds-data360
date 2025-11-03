d2ca3320d94df7eee03ad1347dafe214946404d5
baltilucas <l.pavezv@udd.cl>
2025-11-03 14:42:31 -0300

    Config

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,7 @@ export const db = await mysql.createPool({
   host: process.env.DB_HOST || '54.166.37.210',
   user: process.env.DB_USER || 'ionic_user',
   password: process.env.DB_PASS || 'ionic_pass',
-  database: process.env.DB_NAME || 'ionic',
+  database: process.env.DB_NAME || 'fichasmedicas',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
