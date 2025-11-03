d26bdcc2505dcaf87f830aa0881682580f212b32
baltilucas <l.pavezv@udd.cl>
2025-11-03 17:13:48 -0300

    Perfil paciente

--- Front-Ionic/src/app/datospersonales/datospersonales.page.ts
+++ Front-Ionic/src/app/datospersonales/datospersonales.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule} from '@ionic/angular';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 
 @Component({
@@ -9,16 +10,27 @@ import { IonicModule} from '@ionic/angular';
   templateUrl: './datospersonales.page.html',
   styleUrls: ['./datospersonales.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, HttpClientModule]
 })
 export class DatospersonalesPage implements OnInit {
 
-  constructor(private location: Location) { }
+  paciente: any = {};
+
+  link = 'http://54.166.37.210:4000';
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get<any>(`${this.link}/pacientes/1`).subscribe({
+      next: (data) => {
+        if (data && data.length > 0) {
+          this.paciente = data[0];
+        }
+      },
+      error: (err) => {
+        console.error('Error cargando datos del paciente', err);
+      }
+    });
   }
 
-  goBack() {
-    this.location.back();  
-  }
 }
