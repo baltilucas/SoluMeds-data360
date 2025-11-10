9ff7f21418ab8b4864b217edc58bf12bf9184236
baltilucas <l.pavezv@udd.cl>
2025-11-10 15:43:43 -0300

    .

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db = await mysql.createPool({
-  host: process.env.DB_HOST || '54.166.37.210',
+  host: process.env.DB_HOST || '3.231.209.50',
   user: process.env.DB_USER || 'ionic_user',
   password: process.env.DB_PASS || 'ionic_pass',
   database: process.env.DB_NAME || 'fichasmedicas',
